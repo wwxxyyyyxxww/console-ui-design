cf1507d8122600e976c70b4d7eb70eda53070f5f
xiaoyuer <xiaoyuer6964@163.com>
2022-10-21 09:37:43 +0800

    fix: 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: '@mistjs/eslint-config-vue',
-}
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {},
+};
